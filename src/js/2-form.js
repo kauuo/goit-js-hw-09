8379d129d924952f3d7ed9554d0a447cf5818714
kauuo <bonamix@ukr.net>
2024-05-18 09:26:52 +0300

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -11,8 +11,15 @@ form.addEventListener('submit', handleSubmit);
 emailInput.addEventListener('input', handleInput);
 messageInput.addEventListener('input', handleInput);
 
+populateFormData();
+
 function handleSubmit(event) {
   event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Заповніть обидва поля.');
+    return;
+  }
 
   console.log(formData);
   event.target.reset();
@@ -36,57 +43,3 @@ function populateFormData() {
     Object.assign(formData, parsedFormData);
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-//__________________________________________________//
-
-// const formData = {
-//   email: '',
-//   message: '',
-// };
-
-// const form = document.querySelector('.feedback-form');
-// const emailInput = form.querySelector('input[name="email"]');
-// const messageInput = form.querySelector('textarea');
-
-// form.addEventListener('submit', handleSubmit);
-// emailInput.addEventListener('input', handleInput);
-// messageInput.addEventListener('input', handleInput);
-// populateFormData();
-
-// function handleSubmit(event) {
-//   event.preventDefault();
-
-//   console.log(formData);
-
-//   event.target.reset();
-//   localStorage.removeItem('formData');
-// }
-
-// function handleInput() {
-//   formData.email = emailInput.value;
-//   formData.message = messageInput.value;
-
-//   localStorage.setItem('formData', JSON.stringify(formData));
-// }
-
-// function populateFormData() {
-//   const savedFormData = localStorage.getItem('formData');
-
-//   if (savedFormData) {
-//     const parsedFormData = JSON.parse(savedFormData);
-//     emailInput.value = parsedFormData.email;
-//     messageInput.value = parsedFormData.message;
-//     Object.assign(formData, parsedFormData);
-//   }
-// }
